5d9fb0f7f0b7f6739c2a7071a70d1bba38037aaf
iLazer <Pandqbear@gmail.com>
2025-04-04 15:15:18 -0700

    changed file structure and added tabs system
    made it much easier to work without merge conflicts, and organized project.

---
+++ app/lib/tabs.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'AppUsage.dart';
+
+class Tabs extends StatelessWidget {
+  const Tabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.bar_chart)),
+                Tab(icon: Icon(Icons.one_x_mobiledata)),
+              ],
+            ),
+            title: const Text('Tabs Demo'),
+          ),
+          body: const TabBarView(
+            children: [
+              AppUsageApp(), // ✅ Just call the constructor
+              Center(child: Icon(Icons.directions_transit)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
