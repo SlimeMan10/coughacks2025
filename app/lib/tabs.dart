2996008054c4509caa5fe7db256360a37bd9e252
iLazer <Pandqbear@gmail.com>
2025-04-04 15:42:23 -0700

    new tabs cleaner and nicer
    made them on the bottom and look more aesteticsally pleasing

--- app/lib/tabs.dart
+++ app/lib/tabs.dart
@@ -1,28 +1,49 @@
 import 'package:flutter/material.dart';
-import 'AppUsage.dart';
+import 'AppUsage.dart'; // Make sure this exports AppUsageApp
 
-class Tabs extends StatelessWidget {
+class Tabs extends StatefulWidget {
   const Tabs({super.key});
+
+  @override
+  State<Tabs> createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.bar_chart)),
-                Tab(icon: Icon(Icons.one_x_mobiledata)),
-              ],
-            ),
-            title: const Text('Tabs Demo'),
-          ),
-          body: TabBarView(
-            children: [
-              AppUsageApp(), 
-              const Center(child: Icon(Icons.directions_transit)),
+      home: Scaffold(
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            AppUsageApp(),
+            Center(child: Icon(Icons.directions_transit, size: 64)),
+          ],
+        ),
+        bottomNavigationBar: Material(
+          color: Colors.white60,
+          child: TabBar(
+            controller: _tabController,
+            labelColor: const Color.fromARGB(255, 0, 0, 0),
+            unselectedLabelColor: const Color.fromARGB(153, 83, 82, 82),
+            indicatorColor: Colors.white,
+            tabs: const [
+              Tab(icon: Icon(Icons.bar_chart)),
+              Tab(icon: Icon(Icons.one_x_mobiledata)),
             ],
           ),
         ),
