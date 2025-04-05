fe4b5af92878192301a34c12c799b74754703754
Hugh Gramelspacher <hughgramelspacher@gmail.com>
2025-04-05 06:55:24 -0700

    new bottom tab

--- app/lib/tabs.dart
+++ app/lib/tabs.dart
@@ -2,6 +2,7 @@ import 'package:app/Permissions.dart';
 import 'package:app/LocalLeaderboard.dart';
 import 'package:app/blocking.dart';
 import 'package:app/rulesPage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'AppUsage.dart';
 import 'blockInfo.dart';
@@ -13,20 +14,17 @@ class Tabs extends StatefulWidget {
   State<Tabs> createState() => _TabsState();
 }
 
-class _TabsState extends State<Tabs> with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 6, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
+class _TabsState extends State<Tabs> {
+  int _selectedIndex = 0; // RulesPage is default
+  
+  static final List<Widget> _tabs = [
+    AppUsageApp(),   // Home
+    // BlockInfo(),     // Rules - moved from index 4 to 1 to match tabs order
+    // Blocking(),      // Squads
+    RulesPage(),    
+    PermissionsTab(),
+    LocalLeaderboard(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -42,41 +40,64 @@ class _TabsState extends State<Tabs> with SingleTickerProviderStateMixin {
         textTheme: const TextTheme(
           bodyMedium: TextStyle(color: Colors.white),
         ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Colors.transparent,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.grey,
-        ),
       ),
       home: Scaffold(
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-            AppUsageApp(),
-            PermissionsTab(),
-            Blocking(),
-            RulesPage(),
-            BlockInfo(),
-            LocalLeaderboard(),
-          ],
-        ),
+        body: _tabs[_selectedIndex],
         bottomNavigationBar: Container(
-          decoration: const BoxDecoration(
-            color: Color(0xFF1A1A1A),
-            border: Border(top: BorderSide(color: Colors.white12)),
+          height: 83, // Match the height in screenshot
+          decoration: BoxDecoration(
+            color: const Color(0xFFF8F8F8), // Light background color like in screenshot
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.03),
+                spreadRadius: 0,
+                blurRadius: 0.5,
+                offset: const Offset(0, -0.5),
+              ),
+            ],
           ),
-          child: TabBar(
-            controller: _tabController,
-            indicatorColor: Colors.white,
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.grey,
-            tabs: const [
-              Tab(icon: Icon(Icons.bar_chart)),
-              Tab(icon: Icon(Icons.warning_amber_outlined)),
-              Tab(icon: Icon(Icons.shield_outlined)),
-              Tab(icon: Icon(Icons.handshake)),
-              Tab(icon: Icon(Icons.block)),
-              Tab(icon: Icon(Icons.abc),)
+          child: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.black54,
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            selectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+            unselectedLabelStyle: const TextStyle(
+              fontWeight: FontWeight.w400,
+            ),
+            showUnselectedLabels: true,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.brightness),
+                activeIcon: Icon(CupertinoIcons.brightness_solid),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.lock),
+                activeIcon: Icon(CupertinoIcons.lock_fill),
+                label: 'Rules',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.hand_raised),
+                activeIcon: Icon(CupertinoIcons.hand_raised_fill),
+                label: 'Permissions',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.person),
+                activeIcon: Icon(CupertinoIcons.person_fill),
+                label: 'Leaderboard',
+              ),
             ],
           ),
         ),
